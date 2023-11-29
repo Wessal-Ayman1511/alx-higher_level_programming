5a5ade9782ad28c085905fa177e4c34c0839d166
Wessal Ayman <wessalayman237@gmail.com>
2023-11-28 17:15:43 -0800

    a

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for num1 in range(10):
     for num2 in range(num1, 10):
         if num1 < num2:
             print('{:d}{:d}'.format(num1, num2),
-                end='\n' if num1 == 8 and num2 == 9 else ', ')
+                    end='\n' if num1 == 8 and num2 == 9 else ', ')
