f3358d1732f143547cc808ccd0ea395350b78b8f
Wessal Ayman <wessalayman237@gmail.com>
2023-11-28 16:25:44 -0800

    a

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for alpha in range(ord('a'), ord('z') + 1):
-    if alpha == ord('e') or alpha == ord('q')):
-            continue
-    else:
+    if alpha != ord('e') and alpha != ord('q'):
         print('{:c}'.format(alpha), end='')
