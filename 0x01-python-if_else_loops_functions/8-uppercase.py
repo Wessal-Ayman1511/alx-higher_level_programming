6ac20f2c8a59fd66576fd422e371ffddd558d8b9
Wessal Ayman <wessalayman237@gmail.com>
2023-11-28 17:32:38 -0800

    a

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return (True)
+    else:
+        return (False)
+
+def uppercase(str):
+    for i in str:
+        print('{c}'
+                .format(ord(c)) if not islower(i) else ord(i) - 32,
+                end='')
+        print("")
