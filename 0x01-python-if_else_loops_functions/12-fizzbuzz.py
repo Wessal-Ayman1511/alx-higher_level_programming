b1831bf61e33bef23d0f60c2e78503d73a2ee3e3
Wessal Ayman <wessalayman237@gmail.com>
2023-11-28 18:20:07 -0800

    a

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if (i % 3 == 0 and i % 5 == 0):
+for c in range(1, 101):
+    if (c % 15):
         print('FizzBuzz', end=" ")
-    elif (i % 3 == 0):
+    elif (c % 3 == 0):
         print('Fizz', end=" ")
-    elif (i % 5 == 0):
+    elif (c % 5 == 0):
         print('Buzz', end=" ")
     else:
-        print('{:d}'.format(i), end=" ")
+        print('{:d}'.format(c), end=" ")
