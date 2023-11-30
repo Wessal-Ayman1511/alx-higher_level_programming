0c425f34ca13911a7908002258e4d0f94631795c
Wessal Ayman <wessalayman237@gmail.com>
2023-11-29 21:21:40 -0800

    a

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,10 @@ if __name__ == "__main__":
     import sys
 
     i = len(sys.argv) - 1
-    if(i == 0):
+
+    if i == 0:
         print("0 arguments.")
-    elif(i == 1):
+    elif i == 1:
         print("1 argument:")
     else:
         print(f"{i} arguments:")
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   import sys
-   sum_Arg = 0
-   for i in range(len(sys.argv) - 1):
-       sum_Arg += int(sys.argv[i + 1])
-   print(sum_Arg)
+    import sys
+
+    sum_Arg = 0
+    for i in range(len(sys.argv) - 1):
+        sum_Arg += int(sys.argv[i + 1])
+    print(sum_Arg)
