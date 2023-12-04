3cc23e3eaec1901edef38d3589b235a885d17f62
Wessal Ayman <wessalayman237@gmail.com>
2023-12-04 09:14:55 -0800

    a

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for inner in matrix:
-        if len (inner) == 0:
+        if len(inner) == 0:
             print()
         for j in range(len(inner)):
-            print("{:d}".format(inner[j]),
+            print(
+                    "{:d}"
+                    .format(inner[j]),
                     end='\n' if j == len(inner) - 1 else " ")
