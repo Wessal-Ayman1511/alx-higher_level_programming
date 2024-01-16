fd1f523e39e772eec2373683f278acf3982773fa
Wessal Ayman <wessalayman237@gmail.com>
2024-01-16 01:57:43 -0800

    a

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,4 +4,4 @@ def remove_char_at(str, n):
         if i == n:
             continue
         else:
-            print("{:c}".format(str[i]), end="")
+            print("{:s}".format(str[i]), end="")
