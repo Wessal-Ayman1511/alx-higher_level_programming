61aec187ec9a33c7260b054ff031defbebff9db3
Wessal Ayman <wessalayman237@gmail.com>
2024-01-16 01:50:07 -0800

    a

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 0:
-        print(f"{i:c}", end="")
+        print("{:c}".format(i), end="")
     else:
-        print(f"{(i - 32):c}", end="")
+        print("{:c}".format(i - 32), end="")
