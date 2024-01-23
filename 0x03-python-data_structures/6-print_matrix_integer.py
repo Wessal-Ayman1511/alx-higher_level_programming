2f512d6eeebbd3ec6c4c98985071e70d182d0e8a
Wessal Ayman <wessalayman237@gmail.com>
2024-01-23 03:44:27 -0800

    a

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for sub in matrix:
+        if len(sub) == 0:
+            print()
+        for i in range(len(sub)):
+            print(
+                    "{:d}"
+                    .format(sub[i]), end=" " if i != len(sub) - 1 else '\n')
