06393261c78d2b0051ff29820340e13121cfb24c
Wessal Ayman <wessalayman237@gmail.com>
2024-01-23 04:23:44 -0800

    a

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,4 +2,4 @@
 def multiple_returns(sentence):
     if not sentence:
         return None
-    return (len(sentence), sentence[0])
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
