efc06a9434843899ae78c20fbaae41ef1295304c
Wessal Ayman <wessalayman237@gmail.com>
2024-01-24 11:56:57 -0800

    a

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,10 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    return max(a_dictionary)
+    mx_v = 0
+    mx_k = None
+    for k, v in a_dictionary.items():
+        if v > mx_v:
+            mx_v = v
+            mx_k = k
+    return mx_k
