b967176018f8f3beaccd50fbbcb1aa458103bc98
Wessal Ayman <wessalayman237@gmail.com>
2024-01-24 12:17:32 -0800

    a

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or roman_string == None:
+        return None
+    t = 0
+    n = 0
+    roman = {'M': 1000, 'D': 500, 'C' : 100, 'L': 50, 'X': 10, 'V': 5, 'I' : 1}
+    for i in reversed(roman_string):
+        n = roman[i]
+        t += n if t < n * 5 else -n
+    return t
+
