aed9037a47cc699d27d382cc574b38f4a7f2d3aa
Wessal Ayman <wessalayman237@gmail.com>
2024-02-06 12:02:51 -0800

    a

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,8 @@ class Square:
         Args:
             size: lenngth
         """
-        if type(size) != int:
+        if type(size != int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        self.__size = size
