3a3274e2c403a44a5b10d3ccac933345c86dede6
Wessal Ayman <wessalayman237@gmail.com>
2024-02-06 11:54:21 -0800

    a

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+"""Square Class"""
+
+
+class Square:
+    """Square"""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: lenngth
+        """
+        self.__size = size
