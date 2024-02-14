05b79eb937b31540ab59bddd633644075b2bd1d9
Wessal Ayman <wessalayman237@gmail.com>
2024-02-14 08:55:28 -0800

    Wessal

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    error = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(div) not in (int, float):
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError(error)
+    for rw in matrix:
+        if len(rw) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        if not isinstance(rw, list) or len(rw) == 0:
+            raise TypeError(error)
+        for element in rw:
+            if type(div) not in (int, float):
+                raise TypeError(error)
+    return [[round(elem / div, 2) for elem in rw] for rw in matrix]
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
