a9577fad29910e2f304cda8642b287d7e8a65bf8
Wessal Ayman <wessalayman237@gmail.com>
2024-02-14 04:13:19 -0800

    Wessal

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" module to add two integers """
+
+
+def add_integer(a, b=98):
+    """adds two integers
+
+    Args:
+    a: first int
+    b: second int
+
+    Raises:
+    TypeError if a, b is not int or float
+
+    Return:
+    the addition of the two numbers
+    """
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
