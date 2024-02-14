928e41832b25910a6b5ac7791e3123921b3bfc2c
Wessal Ayman <wessalayman237@gmail.com>
2024-02-14 10:15:46 -0800

    Wessal

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,8 +3,6 @@ def matrix_divided(matrix, div):
     ms_error = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(div, (int, float)):
         raise TypeError('div must be a number')
-    if div == 0:
-        raise ZeroDivisionError('division by zero')
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError(ms_error)
     for rw in matrix:
