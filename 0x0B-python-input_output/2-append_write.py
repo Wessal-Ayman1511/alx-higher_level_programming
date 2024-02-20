a89e1fefe400d0d857a70fbf1777f00102520bec
Wessal Ayman <wessalayman237@gmail.com>
2024-02-20 07:34:07 -0800

    Wessal

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """module to read file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """function that write to file"""
     with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
