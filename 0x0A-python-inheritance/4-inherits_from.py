8f03b0c3653ea5d0c4ed6d095d33eb2d044f0868
Wessal Ayman <wessalayman237@gmail.com>
2024-02-19 16:52:59 -0800

    Wessal

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module for check if the object is exactly an instance of the class
+"""
+
+
+def inherits_from(obj, a_class):
+    """def inherits_from(obj, a_class)"""
+    return isinstance(obj, a_class) and type(obj) != a_class
