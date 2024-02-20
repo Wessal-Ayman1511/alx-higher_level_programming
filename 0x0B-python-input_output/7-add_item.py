a34ea55ed63a6e7b9f07df5dc074a47d1061c09b
Wessal Ayman <wessalayman237@gmail.com>
2024-02-20 08:42:28 -0800

    Wessal

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    data_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+        data_list = []
+for i in range (1, len(sys.argv)):
+    data_list.append(sys.argv[i])
+save_to_json_file(data_list, "add_item.json")
