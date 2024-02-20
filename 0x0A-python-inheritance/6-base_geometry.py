3c5ff1fdd74f29aa0d8ec5cb211f1196a685cd2e
Wessal Ayman <wessalayman237@gmail.com>
2024-02-19 16:57:49 -0800

    Wessal

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module that create empty class"""
+
+
+class BaseGeometry:
+    """create class"""
+    def area(self):
+        """function that raise erreor"""
+        raise TypeError("area() is not implemented")
