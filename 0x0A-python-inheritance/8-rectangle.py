a12af7b3e316156f9c6698c412b133bd97361f47
Wessal Ayman <wessalayman237@gmail.com>
2024-02-19 17:56:47 -0800

    Wessal

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,19 +2,7 @@
 """module that check valdity"""
 
 
-class BaseGeometry:
-    """create class"""
-    def area(self):
-        """function that raise erreor"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """function that check validity"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """derived class"""
