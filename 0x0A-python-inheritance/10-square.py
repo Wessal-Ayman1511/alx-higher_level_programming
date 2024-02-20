7efb7d2e01cff79c3030395a6ca0e0cffb4d46ae
Wessal Ayman <wessalayman237@gmail.com>
2024-02-19 18:22:33 -0800

    Wessal

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,14 +6,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Square(Rectangle):
+    """Square module"""
     def __init__(self, size):
         """constructor"""
-        self.__size = size
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
     def area(self):
+        """Area of Square"""
         return self.__size ** 2
-
-    def __str__(self):
-        return super().__str__()
