afb7e45ca0c7280753526a030cc5976f602a725b
Wessal Ayman <wessalayman237@gmail.com>
2024-02-19 18:26:52 -0800

    Wessal

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module that check valdity"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """Square module"""
+    def __init__(self, size):
+        """constructor"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Area of Square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """str function"""
+        return str("[Square] {}/{}".format(self.__size, self.__size))
