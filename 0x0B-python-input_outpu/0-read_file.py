56bb27d1e75501e15ba52d4e870f8b57f268c6bb
Wessal Ayman <wessalayman237@gmail.com>
2024-02-20 07:20:11 -0800

    Wessal

--- 0x0B-python-input_outpu/0-read_file.py
+++ 0x0B-python-input_outpu/0-read_file.py
@@ -2,5 +2,5 @@
 """module to read file"""
 def read_file(filename=""):
     """function that reas from file"""
-    with open(filename, 'r') as f:
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end='')
