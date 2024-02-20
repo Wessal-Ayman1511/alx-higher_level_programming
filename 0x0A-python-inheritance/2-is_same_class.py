5fab1bcf5cf2cb3de7437d4b65f8f12a2ff8dab5
Wessal Ayman <wessalayman237@gmail.com>
2024-02-19 16:31:19 -0800

    Wessal

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module for getting attrib for object"""
+
+
+def is_same_class(obj, a_class):
+    """function that check if obj ininstance or not"""
+    return isinstance(obj, a_class)
