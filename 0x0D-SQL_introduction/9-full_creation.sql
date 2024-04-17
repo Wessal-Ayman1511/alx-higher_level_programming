7ea638d4ccc322ce8fa27706ad8ee337a887bfdb
Wessal Ayman <wessalayman237@gmail.com>
2024-04-17 09:35:39 +0200

    Wessal

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- full creation of table
+create table second_table (
+id INT,
+name VARCHAR(256),
+score INT
+);
+insert into second_table
+values
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8)
