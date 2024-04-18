00695230b4e551f374677239fac16b650f1ea701
Wessal Ayman <wessalayman237@gmail.com>
2024-04-18 08:09:54 +0200

    Wessal

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
 -- select with join
-select c.id, c.name,  s.name
-from cities as c
-    inner join states as s
-    on c.state_id = s.id
-order by c.id ;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
