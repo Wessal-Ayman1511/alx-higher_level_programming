e77f13c936e179e4b5aca0a67f7e0b7372c44b98
Wessal Ayman <wessalayman237@gmail.com>
2024-04-22 19:21:31 +0200

    Wessal

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
+// prints the first argument passed to it
 
 if (process.argv[2] === undefined) {
     console.log('No argument');
-  }
-else {
+  } else {
     console.log(process.argv[2]);
   }
