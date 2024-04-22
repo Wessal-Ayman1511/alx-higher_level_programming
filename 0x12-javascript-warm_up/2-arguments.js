2a377a9562b10408020c14b5e699465c3de5ebce
Wessal Ayman <wessalayman237@gmail.com>
2024-04-22 19:34:16 +0200

    Wessal

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const argNum = process.argv.length;
-console.log(argNum == 2 ? 'No argument' : argNum == 3 ? 'Argument found' : 'Arguments found');
+console.log(argNum === 2 ? 'No argument' : argNum === 3 ? 'Argument found' : 'Arguments found');
