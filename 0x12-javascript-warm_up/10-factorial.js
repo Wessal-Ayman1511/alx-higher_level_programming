143d6845e912879c258d60dd5451df51e4cbcf82
Wessal Ayman <wessalayman237@gmail.com>
2024-04-22 20:10:35 +0200

    Wessal

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+function factorial(x)
+{
+  if(x == 1)
+  {
+    return (1);
+  }
+  else
+  {
+    return (x * factorial(x - 1));
+  }
+}
+const var1 = parseInt(process.argv[2]);
+if (Number.isNaN(var1)) {
+  console.log(1);
+} else {
+  console.log(factorial(var1));
+}
