db67005e267e4a4b32fc43a960f1d65e8b238c09
Wessal Ayman <wessalayman237@gmail.com>
2024-04-23 22:21:56 +0200

    Wessal

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-module.export = class Square extends require('./5-square.js') {
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
