005930ca47c01f5ee2a452a4bb6faaa380a5e62e
Wessal Ayman <wessalayman237@gmail.com>
2024-04-23 22:13:28 +0200

    Wessal

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+module.export = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('C'.repeat(this.width));
+      }
+    }
+  }
+};
