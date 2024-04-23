a242a8fea5c027cde0302b6ea077d08d4f8b27f0
Wessal Ayman <wessalayman237@gmail.com>
2024-04-23 22:26:06 +0200

    Wessal

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
-exports.nbOccurences = function (list, searchElement)
-{
-    let counter = 0;
-    for (let i in list)
-    {
-        if (searchElement === list[i])
-        {
-            counter++;
-        }
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (const i in list) {
+    if (searchElement === list[i]) {
+      counter++;
     }
-    return counter;
-}
+  }
+  return counter;
+};
