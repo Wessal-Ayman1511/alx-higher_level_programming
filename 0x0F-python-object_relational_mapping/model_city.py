1c90b3a8929969daa8d350337a75d8534aa944d3
Wessal Ayman <wessalayman237@gmail.com>
2024-04-29 16:05:11 +0300

    Wessal

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,5 +12,5 @@ class City(Base):
     """Class of cities table"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
-    name=Column(String(128), nullable=False)
-    state_id=Column(Integer, ForeignKey('states.id'))
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
