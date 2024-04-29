fb774b47021a47665e1d5cbd496b5124f9912495
Wessal Ayman <wessalayman237@gmail.com>
2024-04-29 13:59:52 +0300

    Wessal

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""model state"""
+"""Lists states"""
 
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
 
-class State:
-    """ class state """
+class State(Base):
+    """Class of states table"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
