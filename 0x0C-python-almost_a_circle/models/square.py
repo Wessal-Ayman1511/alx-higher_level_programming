14bb8f613b5aa8e94b11270012b06ccdc0f3334f
Wessal Ayman <wessalayman237@gmail.com>
2024-06-19 14:08:11 +0300

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -41,3 +41,6 @@ class Square(Rectangle):
         else:
             for key, val in kwargs.items():
                 setattr(self, key, val)
+    def to_dictionary(self):
+            """ to dic """
+            return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
