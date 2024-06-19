35069c43e9ef8a5a0e31859621c114ca6ed1a6ff
Wessal Ayman <wessalayman237@gmail.com>
2024-06-19 14:48:11 +0300

    json str

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,7 +16,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ to json str """
         if list_dictionaries is None or not list_dictionaries:
