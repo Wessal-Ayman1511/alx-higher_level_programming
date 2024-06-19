c63c51d82388db709153cc06879fdc6bd1ca6cc8
Wessal Ayman <wessalayman237@gmail.com>
2024-06-19 14:45:43 +0300

    json str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,5 +98,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """ to dic """
-        return {"x": self.x, "width": self.width, "id": self.id,
-                "height": self.height, "y": self.y}
+        return {"id": self.id, "width": self.width,
+                "height": self.height, "x": self.x, "y": self.y}
