4284eaf095e625705d4909f9450edb5ffb496fcf
Wessal Ayman <wessalayman237@gmail.com>
2024-08-12 19:20:58 +0300

    file title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/'
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 request(url + id, function (err, response, body) {
   if (err) {
     console.log(err);
